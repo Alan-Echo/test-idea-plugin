bb5ceed51e6116d0a98c7d9aaa8fac81d8f1d341
GitHub Action <action@github.com>
2024-11-06 15:13:44 +0000

    Template cleanup

--- src/main/kotlin/com/github/alanecho/testideaplugin/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/alanecho/testideaplugin/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.alanecho.testideaplugin.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
